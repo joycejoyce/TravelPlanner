7cf21368c8a01cd37208bf74d961d7ecad22de7e
Joyce <dorith1989@gmail.com>
2021-10-20 15:16:45 +0800

    [MyItineraries] WrongItineraryNameDetail

--- travel-planner/src/features/my-itineraries/ItineraryDetail.js
+++ travel-planner/src/features/my-itineraries/ItineraryDetail.js
@@ -1,13 +1,55 @@
+// MUI
+import { makeStyles } from "@material-ui/core/styles";
+
+// my components
+import { getItinerary } from "./dataHandler";
+import { secondary as secondaryFont } from "../../common/styles/fonts.json";
+import { err as errColor } from "../../common/styles/colors.json";
+
 // React
 import { useParams } from "react-router";
 
+const useStyles = makeStyles((theme) => ({
+    wrong: {
+        textAlign: "center",
+        fontSize: "1.3rem",
+        color: errColor
+    },
+    wrongName: {
+        fontFamily: secondaryFont,
+        fontSize: "2rem",
+        lineHeight: "3rem"
+    }
+}));
+
+function WrongItineraryNameDetail({ name }) {
+    // styles
+    const classes = useStyles();
+
+    return (
+        <div className={classes.wrong}>
+            <div>No itinerary named:</div>
+            <div className={classes.wrongName}>{name}</div>
+        </div>
+    )
+}
+
 export default function ItineraryDetail() {
+    // styles
+    const classes = useStyles();
+
+    // data
     const { itineraryName } = useParams();
+    const itinerary = getItinerary(itineraryName);
+
     return (
         <div>
             <div className={["contents"].join(" ")}>
-                ItineraryDetail:
-                {itineraryName}
+                {
+                    itinerary ?
+                    <></> :
+                    <WrongItineraryNameDetail name={itineraryName} />
+                }
             </div>
         </div>
     );

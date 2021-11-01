7118ecbc617b09dd889da6176ad6909ede3c3fac
Joyce <dorith1989@gmail.com>
2021-11-01 09:50:34 +0800

    [About] LanguageSelect

--- travel-planner/src/features/about/LanguageSelect.js
+++ travel-planner/src/features/about/LanguageSelect.js
@@ -1,3 +1,24 @@
+// MUI
+import { makeStyles } from "@material-ui/styles";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+
+// my components
+import { selectLanguage, changeLanguage } from "./languageSlice.js";
+
+// React
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+const useStyles = makeStyles((theme) => {
+    return ({
+        formControl: {
+            position: "absolute",
+            top: "0",
+            right: "0"
+        }
+    });
+});
+
 export const Language = {
     english: {
         label: "English",
@@ -10,7 +31,37 @@ export const Language = {
 };
 
 export default function LanguageSelect() {
+    // styles
+    const classes = useStyles();
+
+    // data
+    const language = useSelector(selectLanguage);
+
+    // ctrl
+    const dispatch = useDispatch();
+    const handleChange = (e) => {
+        const { value } = e.target;
+        dispatch(changeLanguage(value));
+    };
+
     return (
-        <div></div>
+        <FormControl
+            variant="outlined"
+            className={classes.formControl}
+        >
+            <Select
+                value={language}
+                onChange={handleChange}
+            >
+                {
+                    Object.values(Language).map((lagObj) => {
+                        const { label, value } = lagObj;
+                        return (
+                            <MenuItem value={value}>{label}</MenuItem>
+                        );
+                    })
+                }
+            </Select>
+        </FormControl>
     );
 }

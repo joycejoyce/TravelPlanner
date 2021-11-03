026283acb02f2db84909378adbcb3dfdbcccde03
Joyce <dorith1989@gmail.com>
2021-11-03 09:39:29 +0800

    [About] Accentruate SubTitle

--- travel-planner/src/features/about/components/SubTitle.js
+++ travel-planner/src/features/about/components/SubTitle.js
@@ -6,10 +6,21 @@ import { lightColors } from "../../../common/styles/colors.json";
 
 const useStyles = makeStyles((theme) => {
     return ({
+        subTitle: {
+            height: "50px",
+            width: "80vw",
+            maxWidth: "900px",
+            margin: "0 0 8px 0",
+            background: lightColors.subTitleBK,
+            borderRadius: "25px",
+            paddingLeft: theme.spacing(3),
+            textAlign: "center"
+        },
         text: {
             fontWeight: "bold",
             fontSize: "18px",
-            color: lightColors.navbarBlue
+            color: lightColors.navbarBlue,
+            lineHeight: "50px"
         }
     });
 });
@@ -19,6 +30,8 @@ export default function SubTitle({ text }) {
     const classes = useStyles();
 
     return (
-        <h3 className={classes.text}>{text}</h3>
+        <div className={classes.subTitle}>
+            <h3 className={classes.text}>{text}</h3>
+        </div>
     );
 }
